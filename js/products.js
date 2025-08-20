f0ae81aa6a835e8d5726e109eeafd8ea5281b6b8
Manuela Bastos <163070061+manuelabastos98@users.noreply.github.com>
2025-08-20 19:30:33 -0300

    centrar productos

--- js/products.js
+++ js/products.js
@@ -22,14 +22,12 @@ document.addEventListener("DOMContentLoaded", () => {  // Se supone que con esto
                                     // y acá mete todos los datos al contenedor con la
                                     // sintaxis fea esa del `${objeto.propiedad}`
             contenedor.innerHTML += `  
-                <div class="productos">
-                    <div class="producto">
-                        <img src="${producto.image}" alt="${producto.name}">
-                        <h3>${producto.name}</h3>
-                        <p>${producto.description}</p>
-                        <p><strong>Precio:</strong> ${producto.currency} ${producto.cost}</p>
-                        <p><strong>Vendidos:</strong> ${producto.soldCount}</p>
-                    </div>
+                <div class="producto">
+                    <img src="${producto.image}" alt="${producto.name}">
+                    <h3 class="titulo-producto">${producto.name}</h3>
+                    <p>${producto.description}</p>
+                    <p><strong>Precio:</strong> ${producto.currency} ${producto.cost}</p>
+                    <p><strong>Vendidos:</strong> ${producto.soldCount}</p>
                 </div>
             `;
         });     // hay que cambiar el h3 p y strong dependiendo como quieramos decorar
